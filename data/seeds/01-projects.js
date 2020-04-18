58f6e311bdddca77377a8776547f86700a4d8754
Ken Diehl <diehlkj.99@gmail.com>
2020-04-17 21:56:59 -0400

    Completed seeds 00 and 01

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,10 @@
+const cleaner = require('knex-cleaner');
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('projects').insert([
+    { id: 1, name: 'Node db1', description: 'First db project in node', completed: false },
+    { id: 2, name: 'Node db2', description: 'Second db project in node', completed: false },
+    { id: 3, name: 'Node db3', description: 'Third db project in node', completed: false },
+    { id: 4, name: 'Node db4', description: 'Fourth db project in node', completed: false },
+  ])
 };
