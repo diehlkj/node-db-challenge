76f70729e15dc86929761946a74340a189fb9665
Ken Diehl <diehlkj.99@gmail.com>
2020-04-17 21:15:07 -0400

    knex init. Setup knexfile for seed and migration directories, null as default, and foreign key enforcement

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: { // this is the part that makes the foreign keys do the thing they do... :~)
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    }
+  }
+};
