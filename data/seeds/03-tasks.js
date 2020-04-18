d14e7e70c13e401a0f1b932a46a9f55e9f66055b
Ken Diehl <diehlkj.99@gmail.com>
2020-04-17 22:16:15 -0400

    Completed seed 03

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,13 +1,24 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('tasks').insert([
+    { id: 1, project_id: 1, name: 'Clone db-1 github repo', notes: 'Make sure to fork it first', completed: false },
+    { id: 2, project_id: 1, name: 'Check out the branch', notes: 'firstname-lastname', completed: false },
+    { id: 3, project_id: 1, name: 'do the code', notes: '', completed: false },
+    { id: 4, project_id: 1, name: 'commit and push to origin', notes: 'Dont forget to add new files to be tracked', completed: false },
+    
+    { id: 5, project_id: 2, name: 'Clone db-2 github repo', notes: 'Make sure to fork it first', completed: false },
+    { id: 6, project_id: 2, name: 'Check out the branch', notes: 'firstname-lastname', completed: false },
+    { id: 7, project_id: 2, name: 'do the code', notes: '', completed: false },
+    { id: 8, project_id: 2, name: 'commit and push to origin', notes: 'Dont forget to add new files to be tracked', completed: false },
+    
+    { id: 9, project_id: 3, name: 'Clone db-3 github repo', notes: 'Make sure to fork it first', completed: false },
+    { id: 10, project_id: 3, name: 'Check out the branch', notes: 'firstname-lastname', completed: false },
+    { id: 11, project_id: 3, name: 'do the code', notes: '', completed: false },
+    { id: 12, project_id: 3, name: 'commit and push to origin', notes: 'Dont forget to add new files to be tracked', completed: false },
+    
+    { id: 13, project_id: 4, name: 'Clone db-4 github repo', notes: 'Make sure to fork it first', completed: false },
+    { id: 14, project_id: 4, name: 'Check out the branch', notes: 'firstname-lastname', completed: false },
+    { id: 15, project_id: 4, name: 'do the code', notes: '', completed: false },
+    { id: 16, project_id: 4, name: 'commit and push to origin', notes: 'Dont forget to add new files to be tracked', completed: false },
+  ]);
 };
